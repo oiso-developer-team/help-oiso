acc3ca502472c34c0e0ff9bf1bd93b6dfbc789ab
xyxywan <1074637681@qq.com>
2020-12-22 21:09:00 +0800

    修正域名

--- js/script.js
+++ js/script.js
@@ -106,7 +106,7 @@ $(function() {
             $tips.html('↓↓↓ 复制下面的链接，教伸手党使用百度');
             $('#output').fadeIn();
             //$urlOutput.val(window.location.origin + window.location.pathname + '?q=' + Base64.encode(question)).focus().select();
-            $urlOutput.val('http://b.iou.ink' + '?q=' + Base64.encode(question)).focus().select();
+            $urlOutput.val('http://b.iou.ink/' + '?q=' + Base64.encode(question)).focus().select();
         }
         return false;
     });

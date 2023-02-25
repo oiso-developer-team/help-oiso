9cfc9eac27dceb521d56087d15b5ee2479cb2a6a
George Jiang <86469008+george-jiang-wow@users.noreply.github.com>
2023-02-25 11:38:37 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -63,7 +63,7 @@ $(function() {
                         $kw.val(query.substr(0, i));
                         if (++i > query.length) {
                             clearInterval(typeInterval);
-                            $tips.html('3、点击下“百度一下”按钮');
+                            $tips.html('3、点击下“洛谷一下”按钮');
                             
                             $arrow.removeClass('active').fadeIn().animate({
                                 left: $searchSubmit.offset().left + $searchSubmit.width()  / 2 + 'px',
@@ -73,7 +73,7 @@ $(function() {
                                 $arrow.addClass('active');
                                 
                                 stepTimeout = setTimeout(function () {
-                                    window.location = 'https://www.baidu.com/s?ie=utf-8&wd=' + encodeURIComponent(query);
+                                    window.location = 'https://www.oiso.cf/search/?q=' + encodeURIComponent(query);
                                 }, 1000);
                             });
                         }
@@ -91,7 +91,7 @@ $(function() {
         $arrow.stop().hide();
         $kw.val(query);
         query = false;
-        $tips.html('输入一个问题，然后点击百度一下');
+        $tips.html('输入一个问题，然后点击洛谷一下');
     });
     
     /* 提交 */
@@ -103,10 +103,10 @@ $(function() {
             $tips.html('<span style="color: red">搜了个寂寞？</span>');
             $kw.val('');
         } else {
-            $tips.html('↓↓↓ 复制下面的链接，教伸手党使用百度');
+            $tips.html('↓↓↓ 复制下面的链接，教伸手党使用洛谷');
             $('#output').fadeIn();
             //$urlOutput.val(window.location.origin + window.location.pathname + '?q=' + Base64.encode(question)).focus().select();
-            $urlOutput.val('http://b.iou.ink/' + '?q=' + Base64.encode(question)).focus().select();
+            $urlOutput.val('http://luogu.george-jiang.ga/' + '?q=' + Base64.encode(question)).focus().select();
         }
         return false;
     });
